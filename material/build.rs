db283631bfe8d1c2182b2880f3a2b93983e7623f
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2024-06-08 13:06:47 +0200

    fix: protos generation

--- material/build.rs
+++ material/build.rs
@@ -1,7 +1,5 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let files = glob::glob("../protos/**/zk_material.proto")
-        .unwrap()
-        .collect::<Result<Vec<_>, _>>()?;
+fn generate_material_server() -> Result<(), Box<dyn std::error::Error>> {
+    let files = &["../protos/**/zk_material.proto"];
     let mut config = prost_build::Config::new();
     config.enable_type_names();
     tonic_build::configure()
@@ -9,6 +7,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(false)
         .out_dir("src/grpc")
         .include_file("mod.rs")
-        .compile_with_config(config, files.as_slice(), &["../protos"])?;
+        .compile_with_config(config, files, &["../protos"])?;
     Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    generate_material_server()
 }
